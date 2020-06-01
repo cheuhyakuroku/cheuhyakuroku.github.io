bfe8145a19ea68279a1434c9ca3c5b871d6e31de
cheu <cheuh@cheus-MacBook-Pro.local>
2020-06-01 12:53:39 +0700

    add youtube

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,26 +1,27 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "9158e3e0f11a6c9fc72145516958a0c9",
-"assets/assets/avatar.jpg": "b87987b2e0bad15bc5991352deead749",
-"assets/assets/chplay.png": "4657e2ace0bc51f625071f0f0e7e295f",
-"assets/assets/cover.jpg": "fdace3a03bf0884dad160021ba363662",
-"assets/assets/facebook.png": "021ada146ffb7c1753557ff29618d04c",
-"assets/assets/getapp.png": "24acc7d61f00f8ec31d1fd520e530080",
-"assets/assets/github.png": "d22ee3727a7216019c3848df6eafa024",
-"assets/assets/instagram.png": "26631a4043b14dff84180bdf51c3cacb",
-"assets/assets/twitter.png": "8f35a40403a84631c4125c4f1859c7a6",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "0ef75f06c2ef65098da2f6357a2a1d04",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+  "index.html": "8e7dd4870f50fe92bc4adf6dee7a3b69",
+"/": "8e7dd4870f50fe92bc4adf6dee7a3b69",
+"main.dart.js": "ea4afcb5702a22054bb7cc3407bea071",
 "favicon.png": "fe6bd7ed8aa894fb41f52ae548880ad6",
 "icons/Icon-192.png": "5a7b21437ccaeb6d1db159d2ab08e95c",
 "icons/Icon-512.png": "582025df6fdf9f90ef25e8a570d84aab",
-"index.html": "77de4ed24d98ca46ea7054ec543430fa",
-"/": "77de4ed24d98ca46ea7054ec543430fa",
-"main.dart.js": "57475b072e62d211488881ade1aa740d",
-"manifest.json": "632d3e6c29bacba126f5fedd9d6ed160"
+"manifest.json": "985fa189f074a3e9d626dea7ab9202f4",
+"assets/LICENSE": "8e877eff43c93dd8ff3206296e2289dd",
+"assets/AssetManifest.json": "bbf2db3189c5ba19398d70d94846a388",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/chplay.png": "4657e2ace0bc51f625071f0f0e7e295f",
+"assets/assets/instagram.png": "26631a4043b14dff84180bdf51c3cacb",
+"assets/assets/github.png": "d22ee3727a7216019c3848df6eafa024",
+"assets/assets/avatar.jpg": "b87987b2e0bad15bc5991352deead749",
+"assets/assets/twitter.png": "8f35a40403a84631c4125c4f1859c7a6",
+"assets/assets/cover.jpg": "fdace3a03bf0884dad160021ba363662",
+"assets/assets/getapp.png": "24acc7d61f00f8ec31d1fd520e530080",
+"assets/assets/youtube.png": "76a1053524ef89072062f2b78f92b9e6",
+"assets/assets/facebook.png": "021ada146ffb7c1753557ff29618d04c"
 };
 
 self.addEventListener('activate', function (event) {
